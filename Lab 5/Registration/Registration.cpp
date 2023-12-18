e0a8981ab8dc4095c3e28e690b5eb05a2ac32c69
Maks <MMKovshov@stud.kantiana.ru>
2023-12-18 15:41:38 +0200

    Add Lab 6

---
+++ Lab 5/Registration/Registration.cpp
@@ -0,0 +1,244 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <locale>
+
+
+// определяет общие методы для работы с хранилищем объектов типа T
+// методы помечены как виртуальные => класс наследник должен реализовать их
+template <class T>
+class IRepository {
+public:
+    virtual std::vector<T> GetAll() = 0;
+    virtual void Add(const T& item) = 0;
+    virtual void Remove(const T& item) = 0;
+    virtual void Update(const T& item) = 0;
+};
+
+
+// объект пользователя
+class User {
+private:
+    int id;
+    std::string login;
+    std::string password;
+    std::string name;
+
+public:
+    // конструктор по умолчанию
+    User() : id(0), login(""), password(""), name("") {}
+
+    // конструктор с параметрами
+    User(int id, const std::string& login, const std::string& password, const std::string& name)
+        : id(id), login(login), password(password), name(name) {}
+
+    // геттеры и сеттеры для всех полей класса
+    int getId() const { return id; }
+    void setId(int id) { this->id = id; }
+
+    std::string getLogin() const { return login; }
+    void setLogin(const std::string& login) { this->login = login; }
+
+    std::string getPassword() const { return password; }
+    void setPassword(const std::string& password) { this->password = password; }
+
+    std::string getName() const { return name; }
+    void setName(const std::string& name) { this->name = name; }
+
+    // перегруженные операторы
+    bool operator==(const User& other) const {
+        return (id == other.id && login == other.login && password == other.password && name == other.name);
+    }
+
+    bool operator!=(const User& other) const {
+        return !(*this == other);
+    }
+};
+
+
+// шаблонный интерфейс наследует интерфейс IRepository и добавляет методы для работы с объектами
+template <class T>
+class IUserRepository : public IRepository<T> {
+public:
+    virtual T GetById(int id) = 0;
+    virtual T GetByLogin(const std::string& login) = 0;
+};
+
+//абстрактный класс, определяющий методы для управления пользователями
+class IUserManager {
+public:
+    virtual void SignIn(const User& user) = 0;
+    virtual void SignOut(const User& user) = 0;
+    virtual bool isAuthorized() const = 0;
+};
+
+// интерфейс для работы с объектами пользователей
+template <class T>
+class FileUserRepository : public IRepository<T> {
+private:
+    std::string filename = "users.txt";
+
+private:
+    std::vector<T> GetAll() override {
+        std::vector<T> users;
+        T user;
+        std::ifstream file(filename); // открываем файл
+        // последовательно читаем файл и создаем объекты и помещаем их в вектор 
+        int id;
+        std::string login, password, name;
+        while (file >> id >> login >> password >> name) {
+            user.setId(id);
+            user.setLogin(login);
+            user.setPassword(password);
+            user.setName(name);
+            users.push_back(user);
+        }
+        file.close();
+        return users;
+    }
+
+    void Add(const T& user) override {
+        std::ofstream file(filename, std::ios_base::app); // открываем файл
+        file << user.getId() << " " << user.getLogin() << " " << user.getPassword() << " " << user.getName() << std::endl; // записываем переданный объект
+        file.close();
+
+        std::ofstream lastUserFile("last_user.txt");
+        lastUserFile << user.getId() << " " << user.getLogin() << " " << user.getPassword() << " " << user.getName() << std::endl;
+        lastUserFile.close();
+    }
+
+    void Remove(const T& user) override {
+        std::vector<T> users = GetAll(); // получаем всех пользователей
+        auto it = std::remove(users.begin(), users.end(), user); // удаляем пользователя из вектора
+        users.erase(it, users.end());
+        UpdateFileContent(users); // обновляем файл
+    }
+
+    void Update(const T& user) override {
+        std::vector<T> users = GetAll(); // получаем всех пользователей
+        // находим всех пользователей с указанным id в векторе
+        // заменяет его на переданный объект пользователя
+        for (T& u : users) {
+            if (u.getId() == user.getId()) {
+                u = user;
+                break;
+            }
+        }
+        UpdateFileContent(users); // обновление файла
+
+        std::ofstream lastUserFile("last_user.txt");
+        lastUserFile << user.getId() << " " << user.getLogin() << " " << user.getPassword() << " " << user.getName() << std::endl;
+        lastUserFile.close();
+    }
+
+    User GetById(int id) {
+        std::ifstream file(filename); // открываем файл
+        User user;
+        // последовательно читаем и ищем пользвателя с заданным id
+        while (file >> user.id >> user.login >> user.password >> user.name) {
+            if (user.id == id) {
+                file.close();
+                return user;
+            }
+        }
+        file.close();
+        // Если пользователь с указанным ID не найден, вернем пустого пользователя
+        return User{};
+    }
+
+    User GetByLogin(const std::string& login) {
+        std::ifstream file(filename); // открываем файл
+        User user;
+        // последовательно читаем и ищем пользователя с заданным логином
+        while (file >> user.id >> user.login >> user.password >> user.name) {
+            if (user.login == login) {
+                file.close();
+                return user;
+            }
+        }
+        file.close();
+        // Если пользователь с указанным логином не найден, вернем пустого пользователя
+        return User{};
+    }
+
+private:
+    void UpdateFileContent(const std::vector<T>& users) {
+        std::ofstream file(filename); // открываем файл
+        // записываем пользователей из переданного ветора в файл
+        for (const T& user : users) {
+            file << user.getId() << " " << user.getLogin() << " " << user.getPassword() << " " << user.getName() << std::endl;
+        }
+        file.close();
+    }
+};
+
+class FileUserManager : public IUserManager {
+private:
+    std::string lastLoggedInUserFile = "last_user.txt";
+    std::string usersFile = "users.txt";
+    User lastLoggedInUser;
+
+public:
+    void SignIn(const User& user) override {
+        lastLoggedInUser = user; // сохраняем в качестве последнего вошедшего пользователя
+        SaveLastLoggedInUserToFile(user); // сохраняем информацию о вошедшем пользователе
+    }
+
+    void SignOut(const User& user) override {
+        // соответствует ли переданный пользоватеь последнему вошедшему
+        if (lastLoggedInUser.getId() == user.getId()) {
+            lastLoggedInUser = User{}; // Сбрасываем информацию о последнем вошедшем пользователе
+            ClearLastLoggedInUserFile(); // очищаем файл с информацией о последнем вошедшем пользователе
+        }
+    }
+
+    // проверяем авторизован ли какой-либо пользователь
+    bool isAuthorized() const override {
+        return !lastLoggedInUser.getName().empty();
+    }
+
+private:
+    // сохраняем информацию о пользователе
+    void SaveLastLoggedInUserToFile(const User& user) {
+        std::ofstream file(lastLoggedInUserFile);
+        file << user.getId() << " " << user.getLogin()<< " " << user.getPassword() << " " << user.getName()<< std::endl;
+        file.close();
+    }
+
+    // очищает содержимое файла
+    void ClearLastLoggedInUserFile() {
+        std::ofstream file(lastLoggedInUserFile, std::ofstream::out | std::ofstream::trunc); // Очищаем содержимое файла
+        file.close();
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    // создание репозитория пользователей
+    IRepository<User>* userRepository = new FileUserRepository<User>();
+
+    // добавление пользователей
+    User user1(1, "Пользователь1", "password1", "Катя");
+    userRepository->Add(user1);
+
+    User user2(2, "Пользователь2", "password2", "Саша");
+    userRepository->Add(user2);
+
+    // получение всех пользователей
+    std::vector<User> allUsers = userRepository->GetAll();
+    for (const auto& user : allUsers) {
+        std::cout << "Пользователь: " << user.getName() << std::endl;
+    }
+
+    // удаление пользователя
+    userRepository->Remove(user1);
+
+    // обновление пользователя
+    user2.setName("Александр");
+    userRepository->Update(user2);
+
+    delete userRepository;
+
+    return 0;
+}
---
+++ Lab 6/Fabric.cpp
@@ -0,0 +1,277 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <locale.h>
+
+// Абстрактный класс Control (базовый)
+class Control {
+public:
+    // расположение контролла
+    virtual void setPosition(int x, int y) = 0;
+    // получение расположения контролла
+    virtual std::pair<int, int> getPosition() = 0;
+    virtual ~Control() {}
+};
+
+// Подклассы контролов
+// наследуются от Control и переопределяют его виртуальные методы
+class Form : public Control {
+public:
+    void addControl(Control* control) {
+        std::cout << "В форму добавлен control" << std::endl;
+    }
+
+    // Реализация виртуальных методов
+    void setPosition(int x, int y) override {
+        std::cout << "Вызвался setPosition" << std::endl;
+    }
+
+    std::pair<int, int> getPosition() override {
+        std::cout << "Вызвался getPosition" << std::endl;
+        return { 0, 0 };
+    }
+};
+
+class Label : public Control {
+public:
+    void setText(const std::string& text) {
+        std::cout << "Label текст установлен: " << text << std::endl;
+    }
+
+    std::string getText() {
+        std::cout << "Вызвался Label getText" << std::endl;
+        return "Sample text";
+    }
+
+    void setPosition(int x, int y) override {
+        std::cout << "Вызвался Label setPosition" << std::endl;
+    }
+
+    std::pair<int, int> getPosition() override {
+        std::cout << "Вызвался Label getPosition" << std::endl;
+        return { 0, 0 };
+    }
+};
+
+class TextBox : public Control {
+public:
+    void setText(const std::string& text) {
+        std::cout << "TextBox текст установлен: " << text << std::endl;
+    }
+
+    std::string getText() {
+        std::cout << "Вызвался TextBox getText" << std::endl;
+        return "Sample text";
+    }
+
+    void OnValueChanged() {
+        std::cout << "Значение TextBox изменилось" << std::endl;
+    }
+
+    // Реализация виртуальных методов
+    void setPosition(int x, int y) override {
+        std::cout << "Вызвался TextBox setPosition" << std::endl;
+    }
+
+    std::pair<int, int> getPosition() override {
+        std::cout << "Вызвался TextBox getPosition" << std::endl;
+        return { 0, 0 };
+    }
+};
+
+class ComboBox : public Control {
+public:
+    void setItems(const std::vector<std::string>& items) {
+        std::cout << "Элементы ComboBox установлены: " << std::endl;
+    }
+
+    void setSelectedIndex(int index) {
+        std::cout << "Выбранный индекс в ComboBox selected установлен" << index << std::endl;
+    }
+
+    int getSelectedIndex() {
+        std::cout << "ComboBox возвращает выбранный индекс" << std::endl;
+        return 0;
+    }
+
+    std::vector<std::string> getItems() {
+        std::cout << "Вызвался ComboBox get items" << std::endl;
+        return { "Item 1", "Item 2" };
+    }
+
+    void setPosition(int x, int y) override {
+        std::cout << "Вызвался ComboBox setPosition" << std::endl;
+    }
+
+    std::pair<int, int> getPosition() override {
+        std::cout << "Вызвался ComboBox getPosition" << std::endl;
+        return { 0, 0 };
+    }
+};
+
+class Button : public Control {
+public:
+    void setText(const std::string& text) {
+        std::cout << "Установлен текст Button: " << text << std::endl;
+    }
+
+    std::string getText() {
+        std::cout << "Вызвался Button getText" << std::endl;
+        return "Sample text";
+    }
+
+    void Click() {
+        std::cout << "Button нажалась" << std::endl;
+    }
+
+    void setPosition(int x, int y) override {
+        std::cout << "Вызвался Button setPosition" << std::endl;
+    }
+
+    std::pair<int, int> getPosition() override {
+        std::cout << "Вызвался Button getPosition" << std::endl;
+        return { 0, 0 };
+    }
+};
+
+// Абстрактная фабрика
+// определили методы для создания каждого типа контроллера
+class AbstractFactory {
+public:
+    virtual Form* createForm() = 0;
+    virtual Label* createLabel() = 0;
+    virtual TextBox* createTextBox() = 0;
+    virtual ComboBox* createComboBox() = 0;
+    virtual Button* createButton() = 0;
+    virtual ~AbstractFactory() {}
+};
+
+// Подклассы фабрик для каждой операционной системы
+class WindowsFactory : public AbstractFactory {
+public:
+    Form* createForm() override {
+        return new Form();
+    }
+
+    Label* createLabel() override {
+        return new Label();
+    }
+
+    TextBox* createTextBox() override {
+        return new TextBox();
+    }
+
+    ComboBox* createComboBox() override {
+        return new ComboBox();
+    }
+
+    Button* createButton() override {
+        return new Button();
+    }
+};
+
+class LinuxFactory : public AbstractFactory {
+public:
+    Form* createForm() override {
+        return new Form();
+    }
+
+    Label* createLabel() override {
+        return new Label();
+    }
+
+    TextBox* createTextBox() override {
+        return new TextBox();
+    }
+
+    ComboBox* createComboBox() override {
+        return new ComboBox();
+    }
+
+    Button* createButton() override {
+        return new Button();
+    }
+};
+
+class MacOSFactory : public AbstractFactory {
+public:
+    Form* createForm() override {
+        return new Form();
+    }
+
+    Label* createLabel() override {
+        return new Label();
+    }
+
+    TextBox* createTextBox() override {
+        return new TextBox();
+    }
+
+    ComboBox* createComboBox() override {
+        return new ComboBox();
+    }
+
+    Button* createButton() override {
+        return new Button();
+    }
+};
+
+// использует абстрактную фабрику для создания контроллов и вызова их методов
+class Client {
+public:
+    // создаем экземпляры всех типов контроллов
+    void run(AbstractFactory* factory) {
+        Form* form = factory->createForm();
+        Label* label = factory->createLabel();
+        TextBox* textBox = factory->createTextBox();
+        ComboBox* comboBox = factory->createComboBox();
+        Button* button = factory->createButton();
+
+        // добавляем их на форму
+        form->addControl(label);
+        form->addControl(textBox);
+        form->addControl(comboBox);
+        form->addControl(button);
+
+        // устанавливаем свойства
+        label->setText("Hello, world!");
+        label->setPosition(10, 10);
+
+        textBox->setText("Sample text");
+        textBox->setPosition(20, 20);
+
+        comboBox->setItems({ "Item 1", "Item 2" });
+        comboBox->setPosition(30, 30);
+
+        button->setText("Click me!");
+        button->setPosition(40, 40);
+
+        // Удаление контролов
+        delete form;
+        delete label;
+        delete textBox;
+        delete comboBox;
+        delete button;
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    // Определяем текущую операционную систему
+#ifdef _WIN32
+    WindowsFactory factory;
+#elif __linux__
+    LinuxFactory factory;
+#elif __APPLE__
+    MacOSFactory factory;
+#else
+    std::cerr << "Unsupported operating system" << std::endl;
+    return 1;
+#endif
+
+    // запускаем контроллы, используя соответствующую фабрику
+    Client client;
+    client.run(&factory);
+
+    return 0;
+}
